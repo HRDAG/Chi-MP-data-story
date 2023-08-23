7adfd168faa144d3aeb74a72a92f02f5bf176e4c
Bailey <bailey@hrdag.org>
2023-08-23 16:10:00 -0700

    adding script for import

---
+++ import/src/import.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# vim: set ts=4 sts=0 sw=4 si fenc=utf-8 et:
+# vim: set fdm=marker fmr={{{,}}} fdl=0 foldcolumn=4:
+# Authors:     BP
+# Maintainers: BP
+# Copyright:   2023, HRDAG, GPL v2 or later
+# =========================================
+
+# {{{
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", default=None)
+    parser.add_argument("--output", default=None)
+    args = parser.parse_args()
+    assert Path(args.input).exists()
+    return args
+
+
+def get_logger(sname, file_name=None):
+    logger = logging.getLogger(sname)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s " +
+                                  "- %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
+    stream_handler = logging.StreamHandler(stdout)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    if file_name:
+        file_handler = logging.FileHandler(file_name)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger
+# }}}
+
+# main
+if __name__ == '__main__':
+    # setup logging
+    logger = get_logger(__name__, "output/import.log")
+
+    # arg handling
+    args = get_args()
+    mp = pd.read_parquet(args.input)
+
+    # apply minor filtering to prep output
+    mp = mp.loc[mp.year_occurred < 2023]
+
+    mp.to_parquet(args.output)
